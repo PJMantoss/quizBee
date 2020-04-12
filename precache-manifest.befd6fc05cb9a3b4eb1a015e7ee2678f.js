78689a91c6aad2d0ef1a85c1ae6ce63073b7077d
pjmantoss <joelptoss@gmail.com>
2020-04-12 16:47:27 +0100

    Updates

---
+++ precache-manifest.befd6fc05cb9a3b4eb1a015e7ee2678f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c532b6a9e03097a4b46857b380a0a088",
+    "url": "/quizBee/index.html"
+  },
+  {
+    "revision": "062dd40ba1362f287ddc",
+    "url": "/quizBee/static/css/main.8221f444.chunk.css"
+  },
+  {
+    "revision": "ddbca17d7295c6a673c4",
+    "url": "/quizBee/static/js/2.99c267c4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/quizBee/static/js/2.99c267c4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "062dd40ba1362f287ddc",
+    "url": "/quizBee/static/js/main.b36cdf7f.chunk.js"
+  },
+  {
+    "revision": "8353a155975a52e99edd",
+    "url": "/quizBee/static/js/runtime-main.70090919.js"
+  }
+]);
