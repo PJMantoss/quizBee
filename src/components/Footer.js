9205c00bed8a8da848308611acf0ee2daff78672
pjmantoss <joelptoss@gmail.com>
2020-04-12 15:18:32 +0100

    add Footer

---
+++ src/components/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export const Footer = () => {
+    return (
+        <div>
+            <div id="love">
+                <span>Made With</span>
+                <img src="https://img.icons8.com/dusk/64/000000/like.png" alt="love"/> 
+                <span>by <a href="https://github.com/PJMantoss" target="_blank">Mantoss</a></span>
+            </div>
+        </div>
+    )
+}
